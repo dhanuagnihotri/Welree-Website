ae6df1c601a755ca622e857e321ee24654252ce4
Michael Rooney <mrooney.github@rowk.com>
2015-02-11 01:53:09 -0600

    beautiful abstraction and refactoring of tastypie modal forms

--- website/welree/static/js/javascript.js
+++ website/welree/static/js/javascript.js
@@ -23,3 +23,31 @@ $.fn.serializeObject = function()
     });
     return o;
 };
+
+welree = {}
+welree.tastypie_form_callback = function(e) {
+    e.preventDefault();
+    var form = $(this);
+    if (form.is('button')) { form = form.closest('.modal-tastypie').find('form'); }
+    $.ajax({
+      type: 'POST',
+      url: form.attr('action'),
+      data: JSON.stringify($(form).serializeObject()),
+      processData: false,
+      contentType: 'application/json'
+    })
+    .done(function(status, data, xhr) {
+        $('.modal:visible').modal('hide');
+        window.location.reload(true);
+    })
+    .fail(function(b, c) {
+        console.log('error', arguments);
+        console.log(b, c);
+    })
+}
+
+$(function() {
+    $('.modal-tastypie form').on('submit', welree.tastypie_form_callback);
+    $('.modal-tastypie .btn-primary').on('click', welree.tastypie_form_callback);
+})
+
