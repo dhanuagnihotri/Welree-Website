d6266f229ccfb6e68237e94af6b86240f249a212
Michael Rooney <mrooney.github@rowk.com>
2015-02-13 15:48:24 -0600

    add users to admin site

--- website/welree/admin.py
+++ website/welree/admin.py
@@ -6,3 +6,4 @@ from sorl.thumbnail.admin import AdminImageMixin
 class ImageAdmin(AdminImageMixin, admin.ModelAdmin): pass
 admin.site.register(models.JewelryItem, ImageAdmin)
 admin.site.register(models.JewelryCollection, ImageAdmin)
+admin.site.register(models.CustomUser)
